404fc8be2fdb797b65168a0ca30ef83d00897004
Nicoguid <nicocieplak@gmail.com>
2026-06-17 23:12:43 -0300

    Agregado documentacion codigo

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class Pokemon(models.Model):
-    pokemon_id = models.IntegerField(unique=True)
-    nombre = models.CharField(max_length=100)
-    tipos = models.CharField(max_length=200)
-    altura = models.IntegerField()
-    peso = models.IntegerField()
-    imagen = models.URLField(max_length=200, blank=True)
+    """
+    Modelo que representa un pokemon obtenido de la PokeAPI. Cada instancia corresponde a una fila en la base de datos
+    """
+    pokemon_id = models.IntegerField(unique=True)  # ID de la Pokedex
+    nombre = models.CharField(max_length=100)       # Nombre del pokemon
+    tipos = models.CharField(max_length=200)        # Tipos del pokemon separados por coma
+    altura = models.IntegerField()                  # Altura del pokemon
+    peso = models.IntegerField()                    # Peso del pokemon
+    imagen = models.URLField(max_length=200, blank=True)  # URL de la imagen
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.nombre
